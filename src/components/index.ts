edd2a9af1085ea37693b31a7b3da648a287c3af2
PauloHenriqueSousa2020 <paulohenriquesousa1997@gmail.com>
2025-04-28 16:13:23 -0300

    feat: finish history

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 export { Button } from "./Button";
 export { ExerciseCard } from "./ExerciseCard";
 export { Group } from "./Group";
+export { HistoryCard } from "./HistoryCard";
 export { HomePageHeader } from "./HomePageHeader";
 export { Input } from "./Input";
 export { Loading } from "./Loading";
+export { ScreenHeader } from "./ScreenHeader";
 export { UserPhoto } from "./UserPhoto";
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,46 @@
+// libs
+import { useState } from "react";
+
 // components
-import { Center, Text } from "@gluestack-ui/themed";
+import { Heading, Text, VStack } from "@gluestack-ui/themed";
+import { HistoryCard, ScreenHeader } from "components";
+import { SectionList } from "react-native";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    { title: '25/04/2025', data: ['Puxada frontal', "Remada unilateral"] },
+    { title: '26/04/2025', data: ['Puxada frontal',] }
+  ])
   return (
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={() => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            fontFamily={'$heading'}
+            color={'$gray200'}
+            fontSize={'$md'}
+            mt={'$10'}
+            mb={'$3'}
+          >
+            {section.title}
+          </Heading>
+        )}
+        style={{ paddingHorizontal: 32 }}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color={'$gray100'} textAlign={'center'}>
+            Não há exercícios registrados ainda. {"\n"} Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
   )
 }
